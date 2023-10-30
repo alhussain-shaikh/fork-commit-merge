fa02e5f9a8ec5c28238520889793e6601fdd7dea
Al -Hussain Shaikh <116242196+alhussain-shaikh@users.noreply.github.com>
2023-10-30 20:41:07 +0530

    Update decision_tree.py

--- tasks/python/hard/decision_tree.py
+++ tasks/python/hard/decision_tree.py
@@ -3,3 +3,31 @@
 import numpy as np
 
 # TODO: Implement the Decision Tree Classifier using NumPy
+class DecisionTreeClassifier:
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        self.tree = self._grow_tree(X, y)
+        
+    def _grow_tree(self, X, y):
+        # Placeholder for the tree-growing algorithm
+        pass
+
+    def predict(self, X):
+        # Placeholder for the prediction algorithm
+        pass
+
+# Example usage:
+if __name__ == '__main__':
+    # Sample data (X and y)
+    X = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
+    y = np.array([0, 1, 1, 0])
+    
+    # Create and fit the Decision Tree Classifier
+    clf = DecisionTreeClassifier()
+    clf.fit(X, y)
+    
+    # Make predictions
+    new_data = np.array([[1, 1], [2, 2]])
+    predictions = clf.predict(new_data)
+    print(predictions)
