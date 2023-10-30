d77dcb7969b3253bddb5ac156e851c909565b1d0
Al -Hussain Shaikh <116242196+alhussain-shaikh@users.noreply.github.com>
2023-10-30 20:39:32 +0530

    Update number_sum.py

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,11 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Example usage:
+num1 = 5
+num2 = 3
+result = add_numbers(num1, num2)
+print("The sum of {} and {} is: {}".format(num1, num2, result))
